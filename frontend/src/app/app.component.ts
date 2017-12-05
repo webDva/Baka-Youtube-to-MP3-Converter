669d70bb2bcdc45c5493fff629e07c6efaaeb021
Web D.va <webNoDva@gmail.com>
2017-12-05 18:46:39 -0500

    yatta! frontend communicates with backened after 20 hours since start

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {HttpClient} from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   x = 'あい';
+  
+  convert(videoId: string) {
+      this.http.post('http://localhost:3000/convert/' + videoId, {}).subscribe(data => {
+          
+      });
+  }
+  
+  constructor(private http: HttpClient) {}
 }
