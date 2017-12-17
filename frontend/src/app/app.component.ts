c4a46b987d663488eeef5aed4b618324bf8f8287
Web D.va <webNoDva@gmail.com>
2017-12-16 23:53:53 -0500

    button click effect

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,5 @@
+// ng build --prod --aot=false
+
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../environments/environment';
